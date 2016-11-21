30acfd85cd0c695217d74c1c0ea0331857f560e0
Nyxot <ing.anthonyhernandez@gmail.com>
2016-11-21 13:11:50 -0600

    interface created

---
+++ README.md
@@ -0,0 +1,3 @@
+# Paint_with_Android
+	This is a little proyect with android to learn how to create app paint
+	source: https://code.tutsplus.com/series/create-a-drawing-app-on-android--cms-704
---
+++ app/src/main/java/com/example/nyxot/paint/DrawingView.java
@@ -0,0 +1,23 @@
+package com.example.nyxot.paint;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by Nyxot on 21/11/2016.
+ */
+
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing()
+    {
+        //get drawing area setup for interaction
+    }
+}
