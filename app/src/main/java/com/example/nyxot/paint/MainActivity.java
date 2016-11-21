7e576bc2830486d2ca9f2b77b7ffe45a03de855c
Nyxot <ing.anthonyhernandez@gmail.com>
2016-11-21 14:17:38 -0600

    add change color and draw

--- app/src/main/java/com/example/nyxot/paint/MainActivity.java
+++ app/src/main/java/com/example/nyxot/paint/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.nyxot.paint;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawingView drawView;
+    private ImageButton currPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawView = (DrawingView)findViewById(R.id.drawing);
+
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+    }
+
+    public void paintClicked(View view)
+    {
+        //use chosen color
+        if(view!=currPaint)
+        {
+            //update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+            drawView.setColor(color);
+
+            //reflect in the UI
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint = (ImageButton)view;
+        }
     }
 }
